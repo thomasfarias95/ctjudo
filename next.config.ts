773fa4e9de48181e2924fdfa9a3605a6c24a7be5
Thomas Farias <thomasfarias0995@gmail.com>
2026-03-16 20:46:56 -0300

    "Correção no config para ignorar erros de build."

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
-import type { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Permite o deploy mesmo se houver erros de tipagem (como o 'any')
+    // Permite que o build termine mesmo com erros de tipagem (como o 'any')
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Permite o deploy mesmo se houver avisos do ESLint (como variáveis não usadas)
+    // Permite o deploy mesmo com avisos de variáveis não usadas ou regras de estilo
     ignoreDuringBuilds: true,
   },
 };
